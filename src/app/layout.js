4319805a978b7a5558257fb0020fdcf695cdd086
Abdul Mannan <abdulmannan5497@gmail.com>
2025-03-09 01:03:27 +0500

    Updated layout.js file

--- src/app/layout.js
+++ src/app/layout.js
@@ -19,11 +19,18 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+
+
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+
+        <h1>hello nisarrrrr</h1>
         {children}
       </body>
     </html>
   );
 }
+
+
+
